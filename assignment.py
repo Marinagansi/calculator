58b510d7d917f09bd263cb569a8363537d8ee9fe
NITRO5 <gansimarina@gmail.com>
2021-04-21 18:54:13 +0545

    calculator1

--- assignment.py
+++ assignment.py
@@ -80,15 +80,15 @@ button_multi=Button(cal, text="*",padx=39,pady=20,fg="black", bg="bisque3", comm
 button_div=Button(cal,text="/",padx=40,pady=20,fg="black", bg="bisque3",command=button_div)
 
 button_1.grid(row=1,column=0)
-button_2.grid(row=2,column=0)
-button_3.grid(row=3,column=0)
+button_2.grid(row=1,column=1)
+button_3.grid(row=1,column=2)
 
-button_4.grid(row=1,column=1)
+button_4.grid(row=2,column=0)
 button_5.grid(row=2,column=1)
-button_6.grid(row=3,column=1)
+button_6.grid(row=2,column=2)
 
-button_7.grid(row=1,column=2)
-button_8.grid(row=2,column=2)
+button_7.grid(row=3,column=0)
+button_8.grid(row=3,column=1)
 button_9.grid(row=3,column=2)
 
 button_0.grid(row=4,column=0)
